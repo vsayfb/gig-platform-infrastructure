5982b19baa2946543dbfd82d5549077f97f535c9
vsay <96926401+vsayfb@users.noreply.github.com>
2026-07-11 19:50:12 +0200

    feat: provision SQS queues and least-privilege IAM policies

--- environment/prod/outputs.tf
+++ environment/prod/outputs.tf
@@ -6,3 +6,16 @@ output "nat_instance_public_ip" {
   description = "SSH here to jump into the private subnet."
   value       = module.network.nat_instance_public_ip
 }
+
+output "rds_endpoint" {
+  value = module.data.rds_endpoint
+}
+
+output "rds_master_user_secret_arn" {
+  value = module.data.rds_master_user_secret_arn
+}
+
+output "rds_db_name" {
+  value = module.data.rds_db_name
+}
+
